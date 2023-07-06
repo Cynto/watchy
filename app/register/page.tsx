8222a8b7c8f2331e27ff3374c4c80f250ea486d2
Cynto <lucagaravello@outlook.com>
2023-07-06 17:33:04 +0200

    Add UserRegisterForm component

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,8 @@
-
+import UserRegisterForm from "@app/(components)/UserRegisterForm";
 export default function Register() {
     return (
         <main className="h-screen flex justify-center items-center">
-            <div className="w-1/2 min-w-[280px] max-w-[480px] h-2/3 min-h-fit px-7 py-10 rounded-sm bg-zinc-900">
+            <div className="w-1/2 min-w-[280px] max-w-[480px] h-2/3 min-h-fit flex-col flex gap-y-5  px-7 py-10 rounded-sm bg-zinc-900">
                 <header className="w-full grid gap-2">
                     <h1 className="text-gray-100 text-2xl font-semibold justify-self-center">Join Watchy today!</h1>
                     <span className=" border-b-[2px] border-zinc-700 rounded-3xl"></span>
@@ -12,6 +12,7 @@ export default function Register() {
 
                     </p>
                 </header>
+                <UserRegisterForm />
             </div>
         </main>
     )
