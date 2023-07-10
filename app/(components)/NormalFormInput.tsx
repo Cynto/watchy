642d5f7a7fe3babab1d40a89430c7af7bab36daa
Cynto <lucagaravello@outlook.com>
2023-07-10 17:44:55 +0200

    Add optional props, add variable width to tailwind classes

--- app/(components)/NormalFormInput.tsx
+++ app/(components)/NormalFormInput.tsx
@@ -1,13 +1,15 @@
 
-export default function NormalFormInput({type, required, defaultVal, id, name}: {
+export default function NormalFormInput({type, required, defaultVal, placeholder, id, name, width}: {
     type: string,
-    required: boolean,
-    defaultVal: string
+    required?: boolean,
+    defaultVal?: string
+    placeholder?: string,
     id: string,
-    name:  string
+    name:  string,
+    width?: string
 }) {
     return (
-        <input type={type} required={required} defaultValue={defaultVal} id={id} name={name}
-               className="min-w-full bg-zinc-900  text-gray-100 cursor-text flex text-[0.81rem] h-8 py-1 px-3 w-96 rounded border-[1px] hover:border-[2px] border-zinc-600 focus:outline-none focus:border-[4px] focus:border-red-800"/>
+        <input type={type} required={required} defaultValue={defaultVal} id={id} name={name} placeholder={placeholder}
+               className={`${width ? width : 'min-w-full'} bg-zinc-900  text-gray-100 cursor-text flex text-[0.81rem] h-8 py-1 px-3 w-96 rounded border-[1px] hover:border-[2px] border-zinc-600 focus:outline-none focus:border-[4px] focus:border-red-800`}/>
     )
 }
