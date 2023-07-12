1c0239ee8a0dbfe185b6146f3b9865257f718298
Cynto <lucagaravello@outlook.com>
2023-07-12 17:02:59 +0200

    Create refs for each input, create handleSubmit skeleton function with userPayload containing all values. Add submit button

--- app/(components)/UserRegisterForm.tsx
+++ app/(components)/UserRegisterForm.tsx
@@ -1,16 +1,45 @@
+'use client'
+import {useRef} from "react";
 import NormalFormInput from "@app/(components)/NormalFormInput";
 import LabelWithInput from "@app/(components)/LabelWithInput";
+import {FormEvent} from "react";
 
 export default function UserRegisterForm() {
-    return (<form className="grid gap-4">
-        <LabelWithInput id={"username"} name={"Username"} type={"text"} required/>
-        <LabelWithInput id={"password"} name={"Password"} type={"password"} required/>
-        <LabelWithInput id={"passwordConfirm"} name={"Confirm Password"} type={"password"}  required/>
+    const emailRef = useRef<HTMLInputElement | null>(null)
+    const usernameRef = useRef<HTMLInputElement | null>(null)
+    const passwordRef = useRef<HTMLInputElement | null>(null)
+    const passwordConfirmRef = useRef<HTMLInputElement | null>(null)
+    const dayRef = useRef<HTMLInputElement | null>(null)
+    const monthRef = useRef<HTMLSelectElement | null>(null)
+    const yearRef = useRef<HTMLInputElement | null>(null)
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+
+        const userPayload = {
+            email: emailRef.current?.value,
+            username: usernameRef.current?.value,
+            password: passwordRef.current?.value,
+            passwordConfirm: passwordConfirmRef.current?.value,
+            dob: {
+                day: dayRef.current?.value,
+                month: monthRef.current?.value,
+                year: yearRef.current?.value
+            }
+        };
+
+    }
+
+    return (<form className="grid grid-cols-1 gap-4" onSubmit={handleSubmit}>
+        <LabelWithInput id={"email"} name={"Email"} type={"email"} required innerRef={emailRef}/>
+        <LabelWithInput id={"username"} name={"Username"} type={"text"} required innerRef={usernameRef}/>
+        <LabelWithInput id={"password"} name={"Password"} type={"password"} required innerRef={passwordRef}/>
+        <LabelWithInput id={"passwordConfirm"} name={"Confirm Password"} type={"password"}  required innerRef={passwordConfirmRef}/>
 
         <div className="text-gray-100  text-[0.81rem] grid grid-cols-3 grid-rows-2 gap-x-3">
             <legend className="col-span-full text-neutral-100 font-bold">Date of Birth</legend>
-            <NormalFormInput type={"text"} required={true} defaultVal={""} id={"day"} name={"day"} placeholder={"Day"} width={"w-full"} />
-            <select defaultValue={0} name="month" id={"month"} className={"w-full h-8 py-1 px-3 text-gray-100 bg-zinc-900 rounded border-[1px] hover:border-[2px] border-zinc-600 focus:outline-none focus:border-[4px] focus:border-red-800"}>
+            <NormalFormInput type={"text"} required={true} defaultVal={""} id={"day"} name={"day"} placeholder={"Day"} width={"w-full"} innerRef={dayRef}/>
+            <select ref={monthRef} defaultValue={0} name="month" id={"month"} className={"w-full h-8 py-1 px-3 text-gray-100 bg-zinc-900 rounded border-[1px] hover:border-[2px] border-zinc-600 focus:outline-none focus:border-[4px] focus:border-red-800"}>
                 <option value={0}  disabled>Month</option>
                 <option value={1}>January</option>
                 <option value={2}>February</option>
@@ -25,9 +54,11 @@ export default function UserRegisterForm() {
                 <option value={11}>November</option>
                 <option value={12}>December</option>
             </select>
-            <NormalFormInput type={"text"} required={true} defaultVal={""} id={"year"} name={"year"} placeholder={"Year"} width={"w-full"} />
+            <NormalFormInput type={"text"} required={true} defaultVal={""} id={"year"} name={"year"} placeholder={"Year"} width={"w-full"} innerRef={yearRef}/>
 
         </div>
-        <LabelWithInput id={"email"} name={"Email"} type={"email"} required />
+        <button type={"submit"} className="py-1.5 rounded-sm bg-zinc-800 text-sm" >
+            Sign Up
+        </button>
     </form>)
 }
