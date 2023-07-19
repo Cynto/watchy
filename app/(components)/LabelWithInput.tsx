d8a5361402640909eb7b2f3753eacce90edc880d
Cynto <lucagaravello@outlook.com>
2023-07-19 16:58:23 +0200

    Add error state so input errors can be shown to users

--- app/(components)/LabelWithInput.tsx
+++ app/(components)/LabelWithInput.tsx
@@ -1,28 +1,49 @@
+import { useState, useEffect } from "react";
 import NormalFormInput from "@app/(components)/NormalFormInput";
-import {MutableRefObject} from "react";
+import { MutableRefObject } from "react";
 
 export default function LabelWithInput({
-    id,
-    name,
-    defaultVal,
-    type,
-    placeholder,
-    required,
-    innerRef
-                                       }: {
-    id: string,
-    name: string,
-    defaultVal?: string,
-    type: string,
-    placeholder?: string,
-    required?: boolean
-    innerRef?: MutableRefObject<HTMLInputElement | null>
-
+  id,
+  name,
+  defaultVal,
+  type,
+  placeholder,
+  required,
+  innerRef,
+  inputError,
+  handleChange,
+}: {
+  id: string;
+  name: string;
+  defaultVal?: string;
+  type: string;
+  placeholder?: string;
+  required?: boolean;
+  innerRef?: MutableRefObject<HTMLInputElement | null>;
+  inputError: string;
+  handleChange?: Function;
 }) {
-    return (
-        <div className="text-gray-100  text-[0.81rem] grid grid-cols-1 gap-2">
-            <label htmlFor={id} className="text-neutral-100 font-bold">{name}</label>
-            <NormalFormInput innerRef={innerRef} type={type} required={required} placeholder={placeholder} defaultVal={defaultVal} id={id} name={id} />
-        </div>
-    )
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    setError(inputError);
+  }, [inputError, error, id]);
+  return (
+    <div className="text-gray-100  text-[0.81rem] grid grid-cols-1 gap-2">
+      <label htmlFor={id} className="text-neutral-100 font-bold">
+        {name}
+      </label>
+      <NormalFormInput
+        innerRef={innerRef}
+        type={type}
+        required={required}
+        placeholder={placeholder}
+        defaultVal={defaultVal}
+        id={id}
+        name={id}
+        handleChange={handleChange}
+      />
+      {error && <p className={"text-red-800 text-sm"}>{error}</p>}
+    </div>
+  );
 }
