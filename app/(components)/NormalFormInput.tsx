c40b93dd24d866e36d3307c58f4f0236e02dfd0f
Cynto <lucagaravello@outlook.com>
2023-07-19 16:59:39 +0200

    Add handleChange prop, so when user exits input, the field is validated

--- app/(components)/NormalFormInput.tsx
+++ app/(components)/NormalFormInput.tsx
@@ -1,17 +1,41 @@
-import {MutableRefObject} from "react";
+import { MutableRefObject } from "react";
 
-export default function NormalFormInput({type, required, defaultVal, placeholder, id, name, width, innerRef}: {
-    type: string,
-    required?: boolean,
-    defaultVal?: string
-    placeholder?: string,
-    id: string,
-    name:  string,
-    width?: string,
-    innerRef?: MutableRefObject<HTMLInputElement | null>
+export default function NormalFormInput({
+  type,
+  required,
+  defaultVal,
+  placeholder,
+  id,
+  name,
+  width,
+  innerRef,
+  handleChange,
+}: {
+  type: string;
+  required?: boolean;
+  defaultVal?: string;
+  placeholder?: string;
+  id: string;
+  name: string;
+  width?: string;
+  innerRef?: MutableRefObject<HTMLInputElement | null>;
+  handleChange?: Function;
 }) {
-    return (
-        <input type={type} required={required} defaultValue={defaultVal} id={id} name={name} placeholder={placeholder} ref={innerRef}
-               className={`${width ? width : 'w-full'} bg-zinc-900  text-gray-100 cursor-text flex text-[0.81rem] h-8 py-1 px-3 w-96 rounded border-[1px] hover:border-[2px] border-zinc-600 focus:outline-none focus:border-[4px] focus:border-red-800`}/>
-    )
+  return (
+    <input
+      type={type}
+      required={required}
+      defaultValue={defaultVal}
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      onBlur={() =>
+        handleChange !== undefined ? handleChange(innerRef) : () => {}
+      }
+      ref={innerRef}
+      className={`${
+        width ? width : "w-full"
+      } bg-zinc-900  text-gray-100 cursor-text flex text-[0.81rem] h-8 py-1 px-3 w-96 rounded border-[1px] hover:border-[2px] border-zinc-600 focus:outline-none focus:border-[4px] focus:border-red-800`}
+    />
+  );
 }
