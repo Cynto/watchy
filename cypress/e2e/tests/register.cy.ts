0bf37c7d422a424fd3d238e347ff1616f1dbc604
Cynto <lucagaravello@outlook.com>
2023-07-28 16:51:25 +0200

    Add tests to make sure errors appear when input values aren't valid. Create a test to make sure errors don't appear when input values are valid

--- cypress/e2e/tests/register.cy.ts
+++ cypress/e2e/tests/register.cy.ts
@@ -17,5 +17,52 @@ describe('register page', () => {
     cy.get('form legend').should('contain', 'Date of Birth');
     cy.get('input').should('have.length', 6);
     cy.get('select').should('have.length', 1);
+    cy.get('form button').should('exist');
+  });
+
+  it('displays errors if no values are entered on submit', () => {
+    cy.get('form button').click();
+
+    cy.get('.input-error').should('have.length.at.least', 4);
+  });
+
+  it("displays errors if value don't meet requirements, and user moves onto next input", () => {
+    cy.get('form button').as('submit');
+    cy.contains('label', 'Email').next('input').type('test');
+    cy.contains('label', 'Username').next('input').click().type('1');
+
+    cy.get('.input-error').should('have.length', 1);
+
+    cy.contains('label', 'Password').next('input').click().type('hhh');
+
+    cy.get('.input-error').should('have.length', 2);
+
+    cy.contains('label', 'Confirm Password').next('input').click().type('hh');
+
+    cy.get('.input-error').should('have.length', 3);
+
+    cy.contains('label', 'Password').next('input').click().type('hhhhhhhhh');
+
+    cy.get('.input-error').should('have.length', 4);
+
+    cy.get('@submit').click();
+
+    cy.get('.input-error').should('have.length', 5);
+  });
+
+  it('should not display errors if input values entered are valid', () => {
+    cy.contains('label', 'Email').next('input').type('test@gmail.com');
+    cy.contains('label', 'Username').next('input').click().type('test');
+    cy.contains('label', 'Password').next('input').click().type('Test1245;');
+    cy.contains('label', 'Confirm Password')
+      .next('input')
+      .click()
+      .type('Test1245;');
+    cy.contains('legend', 'Date of Birth').next('input').click().type('1');
+    cy.get('#month').select(1);
+    cy.contains('legend', 'Date of Birth').nextAll('input').eq(1).type('2000');
+    cy.get('form button').click();
+
+    cy.get('.input-error').should('have.length', 0);
   });
 });
